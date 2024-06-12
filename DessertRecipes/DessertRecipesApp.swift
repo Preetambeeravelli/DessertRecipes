04332696b133beec6e3aec9242f19b2c4d8f4cef
Preetam Beeravelli <preetambeeravelli@gmail.com>
2024-06-12 10:53:26 -0500

    Initial Commit

---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by Preetam Beeravelli on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
